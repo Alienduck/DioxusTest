e07b5e3146ccc04d2679edde834939173f2be8e3
Alienduck <oneuillyr@gmail.com>
2026-07-29 01:24:03 +0200

    rework loading queue

--- crates/website/src/pages/landing_page/page.rs
+++ crates/website/src/pages/landing_page/page.rs
@@ -18,6 +18,7 @@ pub fn LandingPage(props: LandingPageProps) -> Element {
     rsx! {
         div {
             class: Styles::landing_page,
+            Title { text: "Lucid Games " }
             canvas {
                 id: "bevy-canvas",
                 style: "position: absolute; inset: 0; width: 100vw; height: 100vh; z-index: -1; pointer-events: auto;",
@@ -27,7 +28,6 @@ pub fn LandingPage(props: LandingPageProps) -> Element {
                 r#type: "module",
                 "import init from '/bevy_bg.js'; init().catch(console.error);"
             }
-            Title { text: "Lucid Games " }
         }
     }
 }
@@ -44,7 +44,7 @@ fn Title(text: &'static str) -> Element {
                 text_len,
                 AnimationConfig::new(AnimationMode::Tween(Tween {
                     duration: Duration::from_secs_f32(text_len * 0.2),
-                    easing: easer::functions::Sine::ease_out,
+                    easing: easer::functions::Sine::ease_in,
                 }))
                 .with_loop(LoopMode::Times(1)),
             );
